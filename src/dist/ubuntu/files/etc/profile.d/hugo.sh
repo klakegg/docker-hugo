2e7af66b6078e7a5b9e188d7dfd6a39afd705e64
Erlend Klakegg Bergheim <erlend@klakegg.net>
2020-05-21 22:44:48 +0200

    Make the different editions share more when using debian and ubuntu images.

--- src/dist/ubuntu/files/etc/profile.d/hugo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Environment configuration triggered by both shell and hugo script.
-
-# Set `pandoc` alias when $HUGO_PANDOC is set
-test "$HUGO_PANDOC" = "" || alias pandoc="$HUGO_PANDOC"
--- src/dist/ubuntu/files/etc/profile.d/hugo_shell.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Environment configuration triggered both shell.
-
-# Change layout of shell prompt
-PS1="\e[1;32mhugo\e[m:\e[1;34m\w\e[m\$ "
-
-# Create alias for `hugo`
-alias h="hugo"
