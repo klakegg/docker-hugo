33aacea6789020ee67beb59cd302a47104c8ac70
Erlend Klakegg Bergheim <erlend@klakegg.net>
2021-01-17 15:52:56 +0100

    Adding sass-embedded.

---
+++ src/files/_script/sass-embedded.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# This file is triggered inside the _base/Dockerfile-base file.
+
+set -e
+set -u
+
+# Variables
+SASS_EMBEDDED_VERSION="1.0.0-beta.6"
+
+# Download
+wget https://github.com/sass/dart-sass-embedded/releases/download/${SASS_EMBEDDED_VERSION}/sass_embedded-${SASS_EMBEDDED_VERSION}-linux-x64.tar.gz \
+  -O /sass-embedded.tar.gz
+
+# Unpack
+mkdir -p /files/usr/local/lib /files/usr/local/bin
+tar -zxvf sass-embedded.tar.gz -C /files/usr/local/lib
+
+# Create symlink
+ln -s /usr/local/lib/sass_embedded/dart-sass-embedded /files/usr/local/bin/dart-sass-embedded
