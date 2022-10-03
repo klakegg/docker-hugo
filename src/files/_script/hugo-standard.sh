30f68b87944b0b86d2b5ea015af77256cc26d243
Erlend Klakegg Bergheim <126939+klakegg@users.noreply.github.com>
2022-10-03 20:45:13 +0000

    Preparing for 0.103.0

--- src/files/_script/hugo-standard.sh
+++ src/files/_script/hugo-standard.sh
@@ -13,27 +13,35 @@ TARGETPLATFORM=${TARGETPLATFORM:-linux/amd64}
 
 if [ "$TARGETPLATFORM" = "linux/amd64" ]; then
     HUGO_ARCH="64bit"
+    HUGOGURU_ARCH="x86_64"
 elif [ "$TARGETPLATFORM" = "linux/arm64" ]; then
     HUGO_ARCH="ARM64"
+    HUGOGURU_ARCH="aarch64"
 elif [ "$TARGETPLATFORM" = "linux/arm/v7" ]; then
     HUGO_ARCH="ARM"
+    HUGOGURU_ARCH="armv7l"
 else
     echo "Unknown build architecture: $TARGETPLATFORM"
     exit 2
 fi
 
+
 # Download binaries from release
-wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-${HUGO_ARCH}.tar.gz
-wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_checksums.txt
+#wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-${HUGO_ARCH}.tar.gz
+#wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_checksums.txt
+wget https://github.com/hugoguru/dist-hugo/releases/download/v${HUGO_VERSION}/hugo-standard-${HUGO_VERSION}-linux-${HUGOGURU_ARCH}.tar.gz
+wget https://github.com/hugoguru/dist-hugo/releases/download/v${HUGO_VERSION}/hugo-${HUGO_VERSION}-checksums-sha256.txt
 
 # Verify checksums
-grep hugo_${HUGO_VERSION}_Linux-${HUGO_ARCH}.tar.gz hugo_${HUGO_VERSION}_checksums.txt | sha256sum -c
+#grep hugo_${HUGO_VERSION}_Linux-${HUGO_ARCH}.tar.gz hugo_${HUGO_VERSION}_checksums.txt | sha256sum -c
+grep hugo-standard-${HUGO_VERSION}-linux-${HUGOGURU_ARCH}.tar.gz hugo-${HUGO_VERSION}-checksums-sha256.txt | sha256sum -c
 
 # Prepare folders
 mkdir -p /files/usr/lib/hugo
 
 # Unpack downloaded content
-tar -zxf hugo_${HUGO_VERSION}_Linux-${HUGO_ARCH}.tar.gz -C /files/usr/lib/hugo
+#tar -zxf hugo_${HUGO_VERSION}_Linux-${HUGO_ARCH}.tar.gz -C /files/usr/lib/hugo
+tar -zxf hugo-standard-${HUGO_VERSION}-linux-${HUGOGURU_ARCH}.tar.gz -C /files/usr/lib/hugo
 
 # Verify executable
 /files/usr/lib/hugo/hugo version
